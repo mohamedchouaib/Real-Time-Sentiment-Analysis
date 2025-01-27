2db0d36dc23dd4a6f2abcde328651c44c7158f1f
medchouaib <118577335+mohamedchouaib@users.noreply.github.com>
2025-01-27 13:57:09 +0100

    Update spark-streaming.py

--- realtime/jobs/spark-streaming.py
+++ realtime/jobs/spark-streaming.py
@@ -1,24 +1,35 @@
 from time import sleep
+
 import pyspark
+import openai
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import from_json, col, when, udf,upper
+from pyspark.sql.functions import from_json, col, when, udf
 from pyspark.sql.types import StructType, StructField, StringType, FloatType
 from config.config import config
-from textblob import TextBlob
 
-# def sentiment_analysis(comment) -> str:
-#     if comment:
-#         analysis = TextBlob(comment)
-#         if analysis.sentiment.polarity >= 0.19:
-#             return "POSITIVE"
-#         elif 0.001<=analysis.sentiment.polarity<0.19 :
-#             return "NEUTRAL"
-#         else:
-#             return "NEGATIVE"
-#     return "Empty"
+def sentiment_analysis(comment) -> str:
+    if comment:
+        openai.api_key = config['openai']['api_key']
+        completion = openai.ChatCompletion.create(
+            model='gpt-3.5-turbo',
+            messages = [
+                {
+                    "role": "system",
+                    "content": """
+                        You're a machine learning model with a task of classifying comments into POSITIVE, NEGATIVE, NEUTRAL.
+                        You are to respond with one word from the option specified above, do not add anything else.
+                        Here is the comment:
+                        
+                        {comment}
+                    """.format(comment=comment)
+                }
+            ]
+        )
+        return completion.choices[0].message['content']
+    return "Empty"
 
 def start_streaming(spark):
-    topic = 'iot_pr'
+    topic = 'customers_review'
     while True:
         try:
             stream_df = (spark.readStream.format("socket")
@@ -28,19 +39,25 @@ def start_streaming(spark):
                          )
 
             schema = StructType([
-                StructField("DateTime", StringType()),
-                StructField("Temp_C", FloatType()),
-                StructField("Rel_Hum", FloatType()),
-                StructField("Wind_Speed_kmh", FloatType()),
-                StructField("Visibility_km", FloatType()),
-                StructField("Press_kPa", FloatType()),
-                StructField("Weather", StringType()),
-                StructField("weather_key", StringType())
+                StructField("review_id", StringType()),
+                StructField("user_id", StringType()),
+                StructField("business_id", StringType()),
+                StructField("stars", FloatType()),
+                StructField("date", StringType()),
+                StructField("text", StringType())
             ])
-            stream_df = stream_df.select(from_json(col('value'),schema).alias("data")).select(("data.*"))                          
-            # query= stream_df.writeStream.outputMode("append").format("console").options(truncate=False).start()## Output each batch for debugging
-            # query.awaitTermination()
-            kafka_df = stream_df.selectExpr("CAST(weather_key AS STRING) AS key", "to_json(struct(*)) AS value")
+
+            stream_df = stream_df.select(from_json(col('value'), schema).alias("data")).select(("data.*"))
+
+            sentiment_analysis_udf = udf(sentiment_analysis, StringType())
+
+            stream_df = stream_df.withColumn('feedback',
+                                             when(col('text').isNotNull(), sentiment_analysis_udf(col('text')))
+                                             .otherwise(None)
+                                             )
+
+            kafka_df = stream_df.selectExpr("CAST(review_id AS STRING) AS key", "to_json(struct(*)) AS value")
+
             query = (kafka_df.writeStream
                    .format("kafka")
                    .option("kafka.bootstrap.servers", config['kafka']['bootstrap.servers'])
@@ -61,6 +78,7 @@ def start_streaming(spark):
         except Exception as e:
             print(f'Exception encountered: {e}. Retrying in 10 seconds')
             sleep(10)
+
 if __name__ == "__main__":
     spark_conn = SparkSession.builder.appName("SocketStreamConsumer").getOrCreate()
 
