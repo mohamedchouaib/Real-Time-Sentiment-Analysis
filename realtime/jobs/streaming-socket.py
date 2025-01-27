7824eb8a5ac02e98cac89ab28bd73427df71a9b8
medchouaib <118577335+mohamedchouaib@users.noreply.github.com>
2025-01-27 13:56:30 +0100

    Update streaming-socket.py

--- realtime/jobs/streaming-socket.py
+++ realtime/jobs/streaming-socket.py
@@ -15,35 +15,33 @@ def send_data_over_socket(file_path, host='spark-master', port=9999, chunk_size=
     print(f"Listening for connections on {host}:{port}")
 
     last_sent_index = 0
-    try:
-        # Load the entire JSON data at once
-        with open(file_path, 'r') as file:
-            data = json.load(file)  # Assuming the data is a JSON array
+    while True:
+        conn, addr = s.accept()
+        print(f"Connection from {addr}")
+        try:
+            with open(file_path, 'r') as file:
+                # skip the lines that were already sent
+                for _ in range(last_sent_index):
+                    next(file)
 
-        while True:
-            conn, addr = s.accept()
-            print(f"Connection from {addr}")
-            try:
-                # Send data in chunks
-                for i in range(last_sent_index, len(data), chunk_size):
-                    chunk = pd.DataFrame(data[i:i+chunk_size])
-                    # Convert 'DateTime' strings to pd.Timestamp
-                    chunk['DateTime'] = pd.to_datetime(chunk['DateTime'])
-                    print(chunk)
-                    for record in chunk.to_dict(orient='records'):
-                        serialize_data = json.dumps(record, default=handle_date).encode('utf-8')
-                        conn.send(serialize_data + b'\n')
-                        time.sleep(5)
-                    
-                    last_sent_index += chunk_size
+                records = []
+                for line in file:
+                    records.append(json.loads(line))
+                    if(len(records)) == chunk_size:
+                        chunk = pd.DataFrame(records)
+                        print(chunk)
+                        for record in chunk.to_dict(orient='records'):
+                            serialize_data = json.dumps(record, default=handle_date).encode('utf-8')
+                            conn.send(serialize_data + b'\n')
+                            time.sleep(5)
+                            last_sent_index += 1
 
-            except (BrokenPipeError, ConnectionResetError):
-                print("Client disconnected.")
-            finally:
-                conn.close()
-                print("Connection closed")
-    except Exception as e:
-        print(f"An error occurred: {e}")
+                        records = []
+        except (BrokenPipeError, ConnectionResetError):
+            print("Client disconnected.")
+        finally:
+            conn.close()
+            print("Connection closed")
 
 if __name__ == "__main__":
-    send_data_over_socket("datasets/Weather_Data.json")
+    send_data_over_socket("datasets/yelp_academic_dataset_review.json")
